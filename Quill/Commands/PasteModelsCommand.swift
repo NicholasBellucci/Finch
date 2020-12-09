16a88115dcfee3028df304fd2cb774c79112e3d1
Nicholas Bellucci <nickdbellucci@gmail.com>
2020-12-09 12:49:51 -0500

    Added data extension for serializing

--- Quill/Commands/PasteModelsCommand.swift
+++ Quill/Commands/PasteModelsCommand.swift
@@ -7,7 +7,7 @@ class PasteModelsCommand: NSObject, XCSourceEditorCommand {
         guard let selection = invocation.buffer.selections.firstObject as? XCSourceTextRange,
               let copiedString = NSPasteboard.general.pasteboardItems?.first?.string(forType: .string) else { return }
 
-        guard let jsonArray = serialize(data: copiedString.data(using: .utf8)!) else {
+        guard let data = copiedString.data(using: .utf8), let jsonArray = data.serialized() else {
             completionHandler(NSError.malformedJSON)
             return
         }
@@ -16,31 +16,5 @@ class PasteModelsCommand: NSObject, XCSourceEditorCommand {
         invocation.buffer.lines.insert(Tree.write(), at: selection.start.line)
 
         completionHandler(nil)
-    }
-}
-
-private extension PasteModelsCommand {
-    /**
-     Serializes the JSON data from a file.
-
-     Will first check for a single key value pair object.
-     If that fails it will look for an array of key value pairs and take use the first object.
-
-     - Parameter data: The JSON data created
-     */
-    func serialize(data: Data) -> [String: Any]? {
-        var object: Any?
-
-        do {
-            object = try JSONSerialization.jsonObject(with: data, options : .allowFragments)
-        } catch {
-            print("Error writing file: \(error.localizedDescription)")
-        }
-
-        if let json = object as? [String: Any] ?? (object as? [[String: Any]])?.first {
-            return json
-        }
-
-        return nil
     }
 }
