a664473eeeba980ee5787c6ac1eb1918e5708544
Nicholas Bellucci <nickdbellucci@gmail.com>
2020-12-09 12:53:21 -0500

    Added more errors

--- Quill/Commands/PasteModelsCommand.swift
+++ Quill/Commands/PasteModelsCommand.swift
@@ -4,10 +4,18 @@ import XcodeKit
 
 class PasteModelsCommand: NSObject, XCSourceEditorCommand {
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void) -> Void {
-        guard let selection = invocation.buffer.selections.firstObject as? XCSourceTextRange,
-              let copiedString = NSPasteboard.general.pasteboardItems?.first?.string(forType: .string) else { return }
+        guard let selection = invocation.buffer.selections.firstObject as? XCSourceTextRange else {
+            completionHandler(NSError.invalidSelection)
+            return
+        }
 
-        guard let data = copiedString.data(using: .utf8), let jsonArray = data.serialized() else {
+        guard let copiedString = NSPasteboard.general.pasteboardItems?.first?.string(forType: .string) else {
+            completionHandler(NSError.emptyClipboard)
+            return
+        }
+
+        guard let data = copiedString.data(using: .utf8),
+              let jsonArray = data.serialized() else {
             completionHandler(NSError.malformedJSON)
             return
         }
--- Quill/Errors/Errors.swift
+++ Quill/Errors/Errors.swift
@@ -8,6 +8,28 @@
 import Foundation
 
 extension NSError {
+    static var emptyClipboard: NSError {
+        NSError(
+            domain: "",
+            code: 1,
+            userInfo: failureReasonInfo(
+                title: "Empty Clipboard",
+                message: "The clipboard appears empty."
+            )
+        )
+    }
+
+    static var invalidSelection: NSError {
+        NSError(
+            domain: "",
+            code: 1,
+            userInfo: failureReasonInfo(
+                title: "Selection Invalid",
+                message: "The selection is invalid."
+            )
+        )
+    }
+
     static var malformedJSON: NSError {
         NSError(
             domain: "",
