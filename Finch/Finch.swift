96db1addaec916339565152ebf0a1d44c1486fc2
Nicholas Bellucci <nickdbellucci@gmail.com>
2020-12-10 16:57:57 -0500

    Added support for swapping between structs based on OSX

--- Finch/Finch.swift
+++ Finch/Finch.swift
@@ -5,9 +5,23 @@
 //  Created by Nicholas Bellucci on 12/8/20.
 //
 
+import Cocoa
 import SwiftUI
 
+var appDelegate = AppDelegate()
+
 @main
+struct AppUserInterfaceSelector {
+    static func main() {
+        if #available(OSX 11.0, *) {
+            Finch.main()
+        } else {
+            FinchOld.main()
+        }
+    }
+}
+
+@available(OSX 11.0, *)
 struct Finch: App {
     var body: some Scene {
         WindowGroup {
@@ -16,4 +30,37 @@ struct Finch: App {
         }
         .windowStyle(HiddenTitleBarWindowStyle())
     }
+}
+
+struct FinchOld {
+    static func main() {
+        NSApplication.shared.setActivationPolicy(.regular)
+
+        let nib = NSNib(nibNamed: NSNib.Name("MainMenu"), bundle: Bundle.main)
+        nib?.instantiate(withOwner: NSApplication.shared, topLevelObjects: nil)
+
+        NSApp.delegate = appDelegate
+        NSApp.activate(ignoringOtherApps: true)
+        NSApp.run()
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var window: NSWindow!
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let contentView = LandingView()
+
+        window = NSWindow(
+            contentRect: NSRect(x: 0, y: 0, width: 800, height: 300),
+            styleMask: [.titled, .closable, .miniaturizable, .resizable, .fullSizeContentView],
+            backing: .buffered, defer: false)
+        window.isReleasedWhenClosed = false
+        window.center()
+        window.setFrameAutosaveName("Main Window")
+        window.contentView = NSHostingView(rootView: contentView)
+        window.makeKeyAndOrderFront(nil)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) { }
 }
